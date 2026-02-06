43cc56e9e2757ce7a1a9391cd04490b718dd531d
Shashank23 <shashankmahaseth2323@gmail.com>
2026-02-06 20:53:50 +0530

    updated

--- App.tsx
+++ App.tsx
@@ -5,9 +5,18 @@
  * @format
  */
 
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { useTheme } from './src/res/themes/hook/useTheme';
+import Splash from './src/presentation/screens/Splash';
 
+import OnBoardingScreen from './src/presentation/screens/OnBoardingScreen';
+import React from 'react';
 
+const App =()=> {
+  const colors =  useTheme()
   return (
+    <SafeAreaProvider  style={[ { backgroundColor: colors.background }]}>
+      <OnBoardingScreen fontColor={colors.text}/>
     </SafeAreaProvider>
   );
 }
